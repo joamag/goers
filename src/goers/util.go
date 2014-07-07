0b9f2451eb78d3590727e499fead88f6fc2f93e8
joamag <joamag@gmail.com>
2014-07-07 20:14:33 +0100

    new comments

--- src/goers/util.go
+++ src/goers/util.go
@@ -2,6 +2,8 @@ package goers
 
 import "code.google.com/p/go-uuid/uuid"
 
+// Generates a UUID value using an external library creating
+// a simple yet usefull abstraction for such operation.
 func UUID() (string, error) {
 	return uuid.New(), nil
 }
