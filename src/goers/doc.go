7e8bdef7788e52decd77eed5113f9f4e97c427d5
joamag <joamag@gmail.com>
2014-07-07 19:13:21 +0100

    new doc

---
+++ src/goers/doc.go
@@ -0,0 +1,5 @@
+// Goers package providers a series of tests for the go language.
+//
+// It's not meant to be used for any particular purpose and should
+// not be used for production systems.
+package goers
