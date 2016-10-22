81f7a5716d0add0d86c12fe1fc7f42d83b7caff4
joamag <joamag@gmail.com>
2016-10-22 14:37:17 +0100

    small comment change

--- src/goers/exec.go
+++ src/goers/exec.go
@@ -3,17 +3,17 @@ package goers
 import "os"
 import "fmt"
 
-// Runs the default execution process that tries to retrieve
-// HTTP information based on the command line arguments and
-// fallsback to the static values otherwise.
+// Execute runs the default execution process that tries to
+// retrieve HTTP information based on the command line
+// arguments and fallsback to the static values otherwise.
 func Execute() {
-    args := os.Args
-    host := DEFAULT_HOST
-    if len(args) > 1 {
-        host = args[1]
-    }
-    err := Get(host)
-    if err != nil {
-        fmt.Println(err)
-    }
+	args := os.Args
+	host := DEFAULT_HOST
+	if len(args) > 1 {
+		host = args[1]
+	}
+	err := Get(host)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
