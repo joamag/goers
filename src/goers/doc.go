e8415ce8063c4ba2e584f6471a33207c9f787dbb
joamag <joamag@gmail.com>
2016-10-22 14:40:44 +0100

    small code change

--- src/goers/doc.go
+++ src/goers/doc.go
@@ -1,4 +1,4 @@
-// Goers package providers a series of tests for the go language.
+// Package goers package providers a series of tests for the go language.
 //
 // It's not meant to be used for any particular purpose and should
 // not be used for production systems.
--- src/goers/util.go
+++ src/goers/util.go
@@ -1,9 +1,9 @@
 package goers
 
-import "code.google.com/p/go-uuid/uuid"
+import "github.com/pborman/uuid"
 
-// Generates a UUID value using an external library creating
-// a simple yet usefull abstraction for such operation.
+// UUID generates a UUID value using an external library
+// creating a simple yet usefull abstraction for such operation.
 func UUID() (string, error) {
-    return uuid.New(), nil
+	return uuid.New(), nil
 }
