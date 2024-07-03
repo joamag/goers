aa4f849ea6788eaf7e238947e10dd1f80e3253ae
João Magalhães <joamag@gmail.com>
2024-07-03 10:51:11 +0100

    chore: new go fmt

--- src/goers/http.go
+++ src/goers/http.go
@@ -1,9 +1,11 @@
 package goers
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
 
 // DefaultPort defines the port that is going to be used in
 // case no other HTTP port is defined
@@ -17,65 +19,65 @@ const DefaultHost = "google.com:80"
 // with the provided host string, the string should conform with
 // the HOST:PORT format to be able to be used correctly.
 func Get(host string) error {
-    // splits the provided host string into the host an port
-    // parts and in case there's no port part it's added using
-    // the default port value defined in the package
-    parts := strings.Split(host, ":")
-    if len(parts) < 2 {
-        host = fmt.Sprintf("%s:%d", host, DefaultPort)
-    }
+	// splits the provided host string into the host an port
+	// parts and in case there's no port part it's added using
+	// the default port value defined in the package
+	parts := strings.Split(host, ":")
+	if len(parts) < 2 {
+		host = fmt.Sprintf("%s:%d", host, DefaultPort)
+	}
 
-    // prints a debug information into the current
-    // command line output indicating the retrieval
-    // of information from the remote server
-    fmt.Printf("GET http://%s\n", host)
+	// prints a debug information into the current
+	// command line output indicating the retrieval
+	// of information from the remote server
+	fmt.Printf("GET http://%s\n", host)
 
-    // establishes the connection to the remote host,
-    // indicating the problem in case it exists
-    conn, err := net.Dial("tcp", host)
-    if err != nil {
-        return err
-    }
+	// establishes the connection to the remote host,
+	// indicating the problem in case it exists
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return err
+	}
 
-    // tries to run a simple get statement on the
-    // current connection, this is hardcoded to
-    // retrieve the root element of the current element
-    _, err = fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-    if err != nil {
-        return err
-    }
-    buffer := bufio.NewReader(conn)
-    status, err := buffer.ReadString((byte)('\n'))
-    if err != nil {
-        return err
-    }
+	// tries to run a simple get statement on the
+	// current connection, this is hardcoded to
+	// retrieve the root element of the current element
+	_, err = fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	if err != nil {
+		return err
+	}
+	buffer := bufio.NewReader(conn)
+	status, err := buffer.ReadString((byte)('\n'))
+	if err != nil {
+		return err
+	}
 
-    // prints the initial status line of the received
-    // request to the standard output buffer
-    fmt.Printf(status)
+	// prints the initial status line of the received
+	// request to the standard output buffer
+	fmt.Print(status)
 
-    // iterates continuously printing the various bytes
-    // that are currently being received
-    for true {
-        // reads a line from the current buffer and in
-        // case the length of the received bytes is
-        // zero, then end od communication has been
-        // reached and must break the current loop
-        line, _, err := buffer.ReadLine()
-        if err != nil {
-            return err
-        }
-        if len(line) == 0 {
-            break
-        }
+	// iterates continuously printing the various bytes
+	// that are currently being received
+	for {
+		// reads a line from the current buffer and in
+		// case the length of the received bytes is
+		// zero, then end od communication has been
+		// reached and must break the current loop
+		line, _, err := buffer.ReadLine()
+		if err != nil {
+			return err
+		}
+		if len(line) == 0 {
+			break
+		}
 
-        // creates a string value out of the line
-        // and then prints to the standard output
-        lineS := string(line[:])
-        fmt.Printf(lineS + "\n")
-    }
+		// creates a string value out of the line
+		// and then prints to the standard output
+		lineS := string(line[:])
+		fmt.Printf(lineS + "\n")
+	}
 
-    // returns an empty error value meaning that no error
-    // as occurred for the current session
-    return nil
+	// returns an empty error value meaning that no error
+	// as occurred for the current session
+	return nil
 }
--- src/goers/util.go
+++ src/goers/util.go
@@ -1,5 +1,3 @@
-//+build full
-
 package goers
 
 import "github.com/pborman/uuid"
@@ -7,5 +5,5 @@ import "github.com/pborman/uuid"
 // UUID generates a UUID value using an external library
 // creating a simple yet useful abstraction for such operation.
 func UUID() (string, error) {
-    return uuid.New(), nil
+	return uuid.New(), nil
 }
