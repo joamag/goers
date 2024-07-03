f1ad7607eaddc7bb647ba55b65b5c0839af563bc
João Magalhães <joamag@gmail.com>
2024-07-03 10:42:44 +0100

    refactor: support for modules

--- src/goers/goers_test.go
+++ src/goers/goers_test.go
@@ -3,17 +3,16 @@ package goers
 import "testing"
 
 func TestSimple(t *testing.T) {
-    result := 42
-    if result != 42 {
-        t.Error("Expected 42, got", result)
-    }
+	result := 42
+	if result != 42 {
+		t.Error("Expected 42, got", result)
+	}
 }
 
 func TestMap(t *testing.T) {
-    var result map[string]int
-    result = make(map[string]int)
-    result["test"] = 42
-    if result["test"] != 42 {
-        t.Error("Expected 42, got", result["test"])
-    }
+	result := make(map[string]int)
+	result["test"] = 42
+	if result["test"] != 42 {
+		t.Error("Expected 42, got", result["test"])
+	}
 }
